eee301a0b25d460b714f2f4044062ef421f2cabd
Jayashree Deshpande <jdeshpan@usc.edu>
2015-06-22 11:06:35 -0400

    Update Test1.java

--- Test1.java
+++ Test1.java
@@ -8,7 +8,7 @@ public class Test1{
 			System.out.println("one");
 		if(s1.equals(s2))
 			System.out.println("two");
-		// Compiler gives error on the following line
+		// Compiler gives error on the following line String and StringBuilder cannot be compared
 		if(s1 == "meo")
 		  System.out.println("three");
 		if(s1.toString() == "meo")
