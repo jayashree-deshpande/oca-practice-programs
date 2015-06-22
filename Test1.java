b0c48c65590e9e5e1c7257a388b5d4385abca9ef
Jayashree Deshpande <jdeshpan@usc.edu>
2015-06-22 11:05:36 -0400

    Create Test1.java

---
+++ Test1.java
@@ -0,0 +1,19 @@
+public class Test1{
+	
+	public static void main(String[] args)
+	{
+		StringBuilder s1 = new StringBuilder("meo");
+		StringBuilder s2 = new StringBuilder("meo");
+		if(s1 == s2)
+			System.out.println("one");
+		if(s1.equals(s2))
+			System.out.println("two");
+		// Compiler gives error on the following line
+		if(s1 == "meo")
+		  System.out.println("three");
+		if(s1.toString() == "meo")
+			System.out.println("four");
+		if(s1.toString().equals(s2))
+			System.out.println("four");
+	}
+}
