ef50843c346d54e6131665cbd9ed823621bf3659
Jayashree Deshpande <jdeshpan@usc.edu>
2015-06-22 11:59:30 -0400

    Create Test3.java

---
+++ Test3.java
@@ -0,0 +1,21 @@
+class Test3{
+	public static void main(String[] args)
+	{
+		final int movieRating = 4;
+		int badMovie = 0;
+		switch(badMovie)
+		{
+			case 0: 
+			case badMovie: System.out.print("aweful");
+			break;
+			case movieRating: System.out.print("Great");
+			break;
+			default:
+			case (int) 'a':
+			case 1*1: System.out.println("==");
+			break;
+		}
+	}
+}
+		
+	
