02ffce01798fb8d6bdf3862e28ae76780d7f1c5e
Jayashree Deshpande <jdeshpan@usc.edu>
2015-06-23 10:42:51 -0400

    Create Test5.java

---
+++ Test5.java
@@ -0,0 +1,15 @@
+class Test5
+{
+	public static void main(String[] args)
+	{
+		char c = 'a';
+		for(int i=1 ; i<=3; i++)
+		{
+			for(int j=0 ; j<=2 ; j++)
+			{
+				System.out.print(c++);
+			}
+		}
+		
+	}
+}
