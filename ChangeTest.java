8007cb6f00e577a49479214ca51cf0a85561b2c4
Jayashree Deshpande <jdeshpan@usc.edu>
2015-08-05 12:35:12 -0400

    Create ChangeTest.java

---
+++ ChangeTest.java
@@ -0,0 +1,19 @@
+public class ChangeTest {
+
+    private int myValue = 0;
+    
+    public void showOne(int myValue){
+        myValue = myValue;
+    }
+    
+    public void showTwo(int myValue){
+        this.myValue = myValue;
+    }    
+    public static void main(String[] args) {
+        ChangeTest ct = new ChangeTest();
+        ct.showOne(100);
+        System.out.println(ct.myValue);
+        ct.showTwo(200);
+        System.out.println(ct.myValue);
+    }
+}
