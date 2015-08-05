7fc9ba4a1dfd4420003205c8a278ac024ec19202
Jayashree Deshpande <jdeshpan@usc.edu>
2015-08-05 12:59:24 -0400

    Create TestClass1.java

---
+++ TestClass1.java
@@ -0,0 +1,14 @@
+class Data {
+    int x = 0, y = 0;
+    public Data(int x, int y){
+        this.x = x; this.y = y;
+    }
+}
+public class TestClass {
+    public static void main(String[] args) throws Exception {
+        Data d = new Data(1, 1);
+        d.x = 2;
+        d.y = 2;
+        System.out.printf("x is="+d.x+" y is="+d.y);
+    }
+}
