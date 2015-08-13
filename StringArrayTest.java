ed1e24de25e133679dbfce399ad894610c4c7a31
Jayashree Deshpande <jdeshpan@usc.edu>
2015-08-13 11:07:02 -0400

    Create StringArrayTest.java

---
+++ StringArrayTest.java
@@ -0,0 +1,7 @@
+public class StringArrayTest{
+   public static void main(String args[]){
+      String[][][] arr  ={{ { "a", "b" , "c"}, { "d", "e", null } },{ {"x"}, null },{{"y"}},{ { "z","p"}, {} }
+      };
+      System.out.println(arr[0][1][2]);
+   }
+}
